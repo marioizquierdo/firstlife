9c3aacd74ea49cc04a4d876de404bec464a5e2b4
Alvin S.J. Ng <alvin.nsj@gmail.com>
2012-06-23 23:05:14 -0700

    added role to user

--- www/model.js
+++ www/model.js
@@ -4,19 +4,44 @@ var dataReference = new Firebase(GAME_LOCATION);
 var usersRef = dataReference.child('users_list');
 var gamesRef = dataReference.child('games_list');
 
+usersRef.on('child_added', function(childSnapshot, prevChildName) {
+	
+});
+
 var FLModel = {
 	createUser: function (userId, userData, callback) {
 
 		usersRef.child(userId).transaction(function(snapshot) {
-
 			if(snapshot===null){
-				return userData;
+				return { role:"", fbData: userData };
 			}
 	  }, function(success, snapshot) {
-	  	callback();
+		
+			usersRef.transaction(function(playerList) {
+
+			    if (playerList === null) {
+			      playerList = [];
+			    }
+					
+					var role = "nice";
+					var size = 0, key;
+					for (key in playerList) {
+						if (playerList.hasOwnProperty(key)) size++;
+					}
+					console.log(size);
+				  if(size-1 % 5 == 0){
+						role = "asshole";
+					}
+					usersRef.child(userId).child("role").set(role);
+					
+			   
+			  }, function (success) {
+			  	callback();
+			  });
 	  });
 
 	},
+	
 	checkIfUserExists: function (userId) {
 	  usersRef.child(userId).once('value', function(snapshot) {
 	    var exists = (snapshot.val() !== null);
