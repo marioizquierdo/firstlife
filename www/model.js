ee8add550ae339168fc471e6ea981c28ff173cc0
Alvin S.J. Ng <alvin.nsj@gmail.com>
2012-06-23 22:14:32 -0700

    test push()

--- www/model.js
+++ www/model.js
@@ -1,33 +1,27 @@
 var GAME_LOCATION = 'http://gamma.firebase.com/firstlife/';
 var dataReference = new Firebase(GAME_LOCATION);
 
-var usersRef = dataReference.child('players_list');
+var usersRef = dataReference.child('users_list');
 var gamesRef = dataReference.child('games_list');
 
 var FLModel = {
 	createUser: function (userId, userData, callback) {
 
 		usersRef.child(userId).transaction(function(snapshot) {
-			if (snapshot === null) {
-				usersRef.transaction(function(snapshot){
-					var user = new Array();
-					user[userId]={
-						name: userData.name,
-						email: userData.email,
-						link: userData.link,
-						gender: userData.gender
-					};
-					usersRef.push(user);
-					callback(userId, userData);
-				});
-			}
 
+			if(snapshot===null){
+				return userData;
+			}
 	  }, function(success, snapshot) {
-				// callback if user_id is not found
-				if(success)
-	    		callback(userId, snapshot);
+	  	callback();
 	  });
 
+	},
+	checkIfUserExists: function (userId) {
+	  usersRef.child(userId).once('value', function(snapshot) {
+	    var exists = (snapshot.val() !== null);
+	    //alert(exists);
+	  });
 	}
 }
 
